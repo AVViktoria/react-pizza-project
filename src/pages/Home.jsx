6e8dd779cfdece3fdc0c667811755e81f2997db0
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-08-11 02:32:04 -0700

    feat: create pagination by react-paginate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,29 +4,28 @@ import Categories from "../components/Categories";
 import Loader from "../components/PizzaBlock/Loader";
 import PizzaBlock from "../components/PizzaBlock/PizzaBlock";
 import Sort from "../components/Sort";
+import Pagination from "../components/Pagination";
 
-const Home = ({searchValue}) => {
+const Home = ({ searchValue }) => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [categoryId, setCategoryId] = useState(555);
+  const [categoryId, setCategoryId] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1)
   const [sortType, setSortType] = useState({
     name: "популярности",
     sortProperty: "rating",
   });
-  
 
   useEffect(() => {
     setIsLoading(true);
-    const sortBy = sortType.sortProperty.replace("-", '');
-    const order = sortType.sortProperty.includes("-") ? 'asc' : 'desc';
+    const sortBy = sortType.sortProperty.replace("-", "");
+    const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
     const category = categoryId > 0 ? `category=${categoryId}` : "";
-    // backend search 
-    const search = searchValue ? `&search=${searchValue}` : '';
-    
+    // backend search
+    const search = searchValue ? `&search=${searchValue}` : "";
+
     fetch(
-      `https://64b78c1321b9aa6eb0784a2e.mockapi.io/items?${
-        category
-      }&sortBy=${sortBy}&order=${order}${search}`
+      `https://64b78c1321b9aa6eb0784a2e.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
     )
       .then((res) => res.json())
       .then((arr) => {
@@ -34,16 +33,19 @@ const Home = ({searchValue}) => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType, searchValue]);
+  }, [categoryId, sortType, searchValue, currentPage]);
 
-  const pizzas = items.filter((obj) => {
-    if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) { return true; } return false;
-}).map((obj) => <PizzaBlock key={obj.id} {...obj}/>);
-  
-  
-  
+  const pizzas = items
+    .filter((obj) => {
+      if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
+        return true;
+      }
+      return false;
+    })
+    .map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+
   const skeleton = [...new Array(6)].map((_, index) => <Loader key={index} />);
-  
+
   return (
     <div className="container">
       <div className="content__top">
@@ -58,14 +60,13 @@ const Home = ({searchValue}) => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
-        {isLoading
-          ? skeleton
-          : pizzas }
+        {isLoading ? skeleton : pizzas}
 
         {/* {items.map((obj) =>
               isLoading ? <Skeleton /> : <PizzaBlock key={obj.id} {...obj} />
             )} */}
       </div>
+      <Pagination onChangePage={number=>setCurrentPage(number) } />
     </div>
   );
 };
