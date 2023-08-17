13a99e2439b1334fb1d7950cc3f699298357b305
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-08-17 11:45:10 -0700

    feat: create url link, pagination,  finish search and sort

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
-import { setCategoryId, setCurrentPage } from "../redux/slices/filterSlice";
+import {
+  setCategoryId,
+  setCurrentPage,
+  setFilters,
+} from "../redux/slices/filterSlice";
 import { SearchContext } from "../App";
 import axios from "axios";
+import qs from "qs";
 
 // components
 import Categories from "../components/Categories";
@@ -10,15 +16,19 @@ import Loader from "../components/PizzaBlock/Loader";
 import PizzaBlock from "../components/PizzaBlock/PizzaBlock";
 import Sort from "../components/Sort";
 import Pagination from "../components/Pagination";
-
+import { list } from "../components/Sort";
 
 const Home = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-
+  const isSearch = useRef(false);
+  const isMounted = useRef(false); //первый рендер
   // const categoryId = useSelector(state => state.filter.categoryId);
   // const sortType = useSelector(state => state.filter.sort.sortProperty);
   //*     можно записать через деструктуризацию
-  const { categoryId, sort, currentPage } = useSelector((state) => state.filter);
+  const { categoryId, sort, currentPage } = useSelector(
+    (state) => state.filter
+  );
   const sortType = sort.sortProperty;
 
   const { searchValue } = useContext(SearchContext); // use hook useContext
@@ -30,10 +40,10 @@ const Home = () => {
     dispatch(setCategoryId(id));
   };
   const onChangePage = (number) => {
-  dispatch(setCurrentPage(number));
-}
-
-  useEffect(() => {
+    dispatch(setCurrentPage(number));
+  };
+  
+  const fetchPizzas = () => {
     setIsLoading(true);
     const sortBy = sortType.replace("-", "");
     const order = sortType.includes("-") ? "asc" : "desc";
@@ -41,14 +51,51 @@ const Home = () => {
 
     // backend search
     const search = searchValue ? `&search=${searchValue}` : "";
-    
-    axios.get(`https://64b78c1321b9aa6eb0784a2e.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`).then(res => {
-      setItems(res.data);
-      setIsLoading(false);
-    })
-       window.scrollTo(0, 0);
-  }, [categoryId, sortType, searchValue, currentPage]);
 
+    axios
+      .get(
+        `https://64b78c1321b9aa6eb0784a2e.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
+      )
+      .then((res) => {
+        setItems(res.data);
+        setIsLoading(false);
+      });
+  };
+
+  // с помощью этого хука создаем строку с параметрами для передачи в ссылку(URL)
+  // если был первый рендер, ссылку не меняем
+  useEffect(() => {
+    if (isMounted.current) {
+      const queryString = qs.stringify({
+        sortProperty: sort.sortProperty,
+        categoryId,
+        currentPage,
+      });
+      navigate(`?${queryString}`);
+    }
+    isMounted.current = true;
+  });
+
+  // при первом рендере парсим параметры, проверяем, есть ли в URL такие параметры
+   // если был первый рендер, то проверяем URL-параметры и сохраняем в Redux
+  useEffect(() => {
+    if (window.location.search) {
+      const params = qs.parse(window.location.search.substring(1));
+      const sort = list.find((obj) => obj.sortProperty === params.sortProperty);
+
+      dispatch(setFilters({ ...params, sort }));
+      isSearch.current = true;
+    }
+  });
+
+  // если был первый рендер, то запрашиваем пиццы
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    if (!isSearch.current) {
+      fetchPizzas();
+    }
+    isSearch.current = false;
+  });
 
   const pizzas = items
     .filter((obj) => {
